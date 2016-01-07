6b5fe4b229a78792d6401e50c3b47636566e90a4
Jesper Ek <deadbeef84@gmail.com>
2016-01-08 00:00:36 +0100

    WIP

--- client/index.js
+++ client/index.js
@@ -9,10 +9,19 @@ socket.on('init', state => {
   console.log(state)
   const tree = new Baobab(state)
   socket.on('update', transactions => {
+    console.log(transactions)
     transactions
+      .map(({type, value, path}) => type === 'push'
+        ? {type, value, path: path.slice(0, -1)}
+        : {type, value, path})
       .forEach(({type, value, path}) => tree.update(path, {type, value}))
   })
   const main = document.getElementById('main')
-  tree.select('time').on('update', ({target}) => main.textContent = target.get())
-  tree.on('update', x => console.log('!!!', x))
+  // tree.select('time').on('update', ({target}) => main.textContent = target.get())
+  tree.on('update', x => main.textContent = JSON.stringify(x.data.currentData))
+
+  socket.on('self', path => {
+    console.log(path)
+    setTimeout(() => socket.emit('update', {ready: true}), 2000)
+  })
 })
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import {createServer} from 'http'
 import socketio from 'socket.io'
 import Game from './game'
+import Room from './room'
 
 const app = express()
 const server = createServer(app)
@@ -20,20 +21,29 @@ import Baobab from 'baobab'
 const tree = new Baobab({
   time: Date.now(),
   rooms: {},
-  players: {},
 })
 
+const rooms = {}
+let roomId = 0
+function addRoom(options) {
+  const id = ++roomId
+  const cursor = tree.select(['rooms', id])
+  rooms[id] = new Room(cursor, options)
+  return rooms[id]
+}
+
+addRoom({name: 'a'})
+
 tree.on('update', ({data}) => {
-  console.dir(data, {depth:5})
+  // console.dir(data, {depth:5})
   io.emit('update', data.transaction)
 })
-// tree.set(['players', 'test'], {name: 'Jesper'})
-// tree.set(['players', 'test2'], {name: 'Jesper'})
-// tree.set(['players', 'test'], {name: 'Jesper Ek'})
 
 io.on('connection', socket => {
   console.log('Client connected!')
   socket.emit('init', tree.get())
+  const player = rooms[1].join(socket, {name: 'Jesper'})
+  socket.emit('self', player.cursor.path)
 })
 
 setInterval(() => tree.set('time', Date.now()), 1000)
